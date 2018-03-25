e81eadf18f69fe68fdf58bbb7b005c87eb1652e2
ssshivm670 <ssshivam670@gmail.com>
2018-03-25 18:22:50 +0530

    Initial build

---
+++ app/src/main/java/com/chat/vijaya/vchatterbox/UserDetails.java
@@ -0,0 +1,10 @@
+package com.chat.vijaya.vchatterbox;
+
+/**
+ * Created by ssshi on 25/3/2018.
+ */
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
